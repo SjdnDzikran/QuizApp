12e53d966844f577e32969a66afab17a4f3f2ce4
Dzikran Azka Sajidan <sjdn.dzikran@gmail.com>
2025-01-24 09:51:10 +0700

    restructure files and folders

--- lib/pages/results_screen.dart
+++ lib/pages/results_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:quiz_app/data/questions.dart';
-import 'package:quiz_app/summary.dart';
+import 'package:quiz_app/widgets/summary.dart';
 
 class ResultsScreen extends StatelessWidget {
   const ResultsScreen({
